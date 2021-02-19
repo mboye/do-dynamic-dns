512e16123b7464f596bfd3defb6842816ea754be
Magnus Boye <magnusboye@gmail.com>
2021-02-19 21:22:21 +0200

    Log incoming HTTP requests

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import Koa from "koa";
+import Koa, { Context } from "koa";
 import config from "./config";
+import morgan from "koa-morgan";
 
 const app = new Koa();
+app.use(morgan("combined"));
 
-app.use((ctx) => {
+app.use(async (ctx) => {
+  if (ctx.path === "/update") {
+    return updateHandler(ctx);
+  }
+
   ctx.body = "Hello world";
 });
+
+const updateHandler = (ctx: Context) => {
+  console.log(ctx.request.toJSON());
+  ctx.status = 200;
+};
 
 console.log(`Listening on port ${config.listenPort}`);
 app.listen(config.listenPort);
