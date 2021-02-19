0c0c5db993a9a12ccf7fe3a1c76030bba1ea7f8b
Magnus Boye <magnusboye@gmail.com>
2021-02-19 21:47:51 +0200

    Add basic authentication

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,16 @@ import { config } from "dotenv";
 
 config();
 
+const requireEnv = (key: string): string => {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable undefiend: ${key}`);
+  }
+  return value;
+};
+
 export default {
   listenPort: process.env.PORT,
+  apiUser: requireEnv("API_USER"),
+  apiPassword: requireEnv("API_PASSWORD"),
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Koa, { Context } from "koa";
 import config from "./config";
 import morgan from "koa-morgan";
+import koaBasicAuth from "koa-basic-auth";
 
 const app = new Koa();
 app.use(morgan("combined"));
+app.use(koaBasicAuth({ name: config.apiUser, pass: config.apiPassword }));
 
 app.use(async (ctx) => {
   if (ctx.path === "/update") {
@@ -15,6 +17,11 @@ app.use(async (ctx) => {
 
 const updateHandler = (ctx: Context) => {
   console.log(ctx.request.toJSON());
+  const { hostname, myip: currentIpAddress } = ctx.request.query;
+  console.log(
+    `Received update. Hostname: ${hostname}, IP address: ${currentIpAddress}`
+  );
+
   ctx.status = 200;
 };
 
